ceb4f746f9ef331bad59daba3e67f1e877055b47
Youssef Slim <youssefslim@Youssefs-MacBook-Pro.local>
2022-09-09 08:58:31 +0200

    refactoring : render method | findBiggestAreas method

--- matrix.js
+++ matrix.js
@@ -11,7 +11,7 @@ const clone = (variable) => {
 // Generate a matrix
 const generateMatrix = (width, height, nbColors) => {
   const matrix = [];
-  console.log("matrix original", matrix);
+
   // List of base colors to choose the number
   //of colors when generating the matrix
   const colors = [
@@ -67,15 +67,12 @@ function checkPixel(matrix, x, y, area) {
 
   // pass the cell already counted to avoid an infinite recursion loop
   matrix[y][x] = false;
-  let count = 0;
 
   // check right pixel color
   if (x < row.length - 1) {
     const rightPixel = matrix[y][x + 1];
 
     if (rightPixel === color) {
-      count++;
-
       // Find the area that has the same color and store it in the array
       area.push({
         x: x + 1,
@@ -84,7 +81,7 @@ function checkPixel(matrix, x, y, area) {
       });
 
       // The function calls itself to check all pixels to the right
-      count += checkPixel(matrix, x + 1, y, area);
+      checkPixel(matrix, x + 1, y, area);
     }
   }
 
@@ -92,40 +89,46 @@ function checkPixel(matrix, x, y, area) {
   if (y < matrix.length - 1) {
     const bottomPixel = matrix[y + 1][x];
     if (bottomPixel === color) {
-      count++;
-
       area.push({
         x: x,
         y: y + 1,
         color: color,
       });
       // The function calls itself to check all pixels to the bottom
-      count += checkPixel(matrix, x, y + 1, area);
+      checkPixel(matrix, x, y + 1, area);
     }
   }
 
   // check left pixel color
   if (x > 0) {
+    // check pixel color
+    const test = (direction, x, y) => {
+      if (direction === color) {
+        area.push({
+          x: x - 1,
+          y: y,
+          color: color,
+        });
+        // The function calls itself to check all pixels to the left
+        checkPixel(matrix, x - 1, y, area);
+      }
+    };
     const leftPixel = matrix[y][x - 1];
     if (leftPixel === color) {
-      count++;
-
       area.push({
         x: x - 1,
         y: y,
         color: color,
       });
       // The function calls itself to check all pixels to the left
-      count += checkPixel(matrix, x - 1, y, area);
+      checkPixel(matrix, x - 1, y, area);
     }
   }
 
   // check top pixel color
   if (y > 0) {
     const topPixel = matrix[y - 1][x];
-
     if (topPixel === color) {
-      count++;
       area.push({
         x: x,
         y: y - 1,
@@ -133,18 +136,14 @@ function checkPixel(matrix, x, y, area) {
       });
 
       // The function calls itself to check all pixels to the top
-      count += checkPixel(matrix, x, y - 1, area);
+      checkPixel(matrix, x, y - 1, area);
     }
   }
-
-  return count;
 }
 
 // extract all areas withe same colors
 function findAreas(originalMatrix) {
   const matrix = clone(originalMatrix);
-  console.log("matrix dans fimdareas", matrix);
-  console.log("originalMatrix; dans fimdareas", originalMatrix);
 
   const areas = [];
   matrix.forEach((row, y) => {
@@ -164,86 +163,56 @@ function findAreas(originalMatrix) {
       areas.push(area);
     });
   });
-  console.log("areas", areas);
+
   return areas;
 }
 
 // Find the biggest area
-function findBiggestAreas(matrix) {
+function findBiggestAreas(orginalMatrix) {
+  const matrix = clone(orginalMatrix);
   const areas = findAreas(matrix);
-  let biggestAreas = [];
+  let lengthOfAreas = [];
 
-  let maxSize = 0;
-  areas.forEach((area) => {
-    if (area.length >= maxSize) {
-      maxSize = area.length;
-    }
+  areas.map((area, i) => {
+    lengthOfAreas = [...lengthOfAreas, area.length];
   });
+  let maxLength = Math.max(...lengthOfAreas);
 
-  areas.forEach((area) => {
-    if (area.length === maxSize) {
-      biggestAreas.push(area);
-    }
-  });
-  console.log("biggestAreas", biggestAreas);
-  return biggestAreas;
-}
+  const biggestAreas = areas.filter((area) => area.length === maxLength);
 
-// highlight the large areas
-function highlightArea(matrix, area, color) {
-  area.forEach((cell) => {
-    matrix[cell.y][cell.x] = color;
-  });
+  biggestAreas.map((area) =>
+    area.map((cell) => (matrix[cell.y][cell.x] = "magenta"))
+  );
 
   return matrix;
 }
 
-// highlight the large areas
-function highlightAreas(matrix, areas) {
-  let highlightedMatrix = clone(matrix);
-
-  areas.forEach((area) => {
-    highlightedMatrix = highlightArea(highlightedMatrix, area, "magenta");
-  });
-
-  return highlightedMatrix;
-}
-
 // draw the matrix in the dom
 const render = () => {
-  let initialWidth = document.querySelector("#width").value;
-  let initialHeight = document.querySelector("#height").value;
-  let initialColor = document.querySelector("#color").value;
+  //initialize an object to store input values
+  let values = {};
 
-  const matrix = generateMatrix(initialWidth, initialHeight, initialColor);
+  // target all inputs in the Dom
+  const inputs = document.querySelectorAll(".input");
+
+  //  get the values from inputs
+  inputs.forEach(({ name, value }) => {
+    values = { ...values, [name]: value };
+  });
+
+  let { width, height, color } = values;
+
+  const matrix = generateMatrix(width, height, color);
   const biggestAreas = findBiggestAreas(matrix);
-  const highlightedMatrix = highlightAreas(matrix, biggestAreas);
 
-  drawMatrixDom(highlightedMatrix);
+  drawMatrixDom(biggestAreas);
 
   document.querySelector("form").addEventListener("submit", (e) => {
     e.preventDefault();
-
-    width = e.target.width.value;
-    height = e.target.height.value;
-    color = e.target.color.value;
-
-    reRender([width, height, color]);
+    const { name, value } = e.target;
+    values = { [name]: value };
+    location.reload();
   });
-};
-
-// update rendering
-const reRender = (args) => {
-  const previousMatrixElement = document.querySelector(".matrix");
-  previousMatrixElement.style.filter = "blur(3px)";
-
-  const matrix = generateMatrix(...args);
-  const biggestAreas = findBiggestAreas(matrix);
-  const highlightedMatrix = highlightAreas(matrix, biggestAreas);
-
-  previousMatrixElement.parentNode.removeChild(previousMatrixElement);
-
-  drawMatrixDom(highlightedMatrix);
 };
 
 render();
